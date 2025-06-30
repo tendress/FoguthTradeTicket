4d7900b367f08a89dac65c98813398971843f529
tendress <anthony.endress@gmail.com>
2025-06-30 16:46:31 -0400

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ st.header("New Trade Ticket")
 # We're adding tickets via an `st.form` and some input widgets. If widgets are used
 # in a form, the app will only rerun once the submit button is pressed.
 with st.form("add_ticket_form"):
-    issue = st.selectbox("Household", households, index=0, placeholder="Select a household")
+    issue = st.selectbox("Household", households, placeholder="Select a household")
     priority = st.selectbox("Priority", ["High", "Medium", "Low"])
     submitted = st.form_submit_button("Submit")
 
